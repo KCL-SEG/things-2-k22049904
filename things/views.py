82921becdf5d9d479171d8b37493347171e74854
Emre Ozbek <mehmet.ozbek@kcl.ac.uk>
2023-10-15 15:45:28 +0100

    Finsihed the assignment

--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from django.http import HttpResponse
+
 
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
